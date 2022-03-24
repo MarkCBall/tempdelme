0db07313e7a20a91dc1be24e9ceee86901d1150e
Mark Camball <mark.camball@gmail.com>
2022-03-24 16:12:04 -0400

    remove velox supplied eth

--- src/tokenSearch/helpers/config.js
+++ src/tokenSearch/helpers/config.js
@@ -1,10 +1,5 @@
 
-export const tokenSyncUri = String(
-  process.env.REACT_APP_TOKEN_SYNC_URI ||
-    'https://testvelox-pj4px6fmua-uc.a.run.app/v1/graphql'
-).replace('ws', 'http');
-
 export const romeTokenSyncUri = String(
-  process.env.REACT_APP_HASURA_API_ENDPOINT_WS ||
+  process.env.REACT_APP_HASURA_API_ENDPOINT_WS ||//todo should be a prop or something
     'https://romenet.prod.velox.global/v1/graphql'
 ).replace('ws', 'http');
--- src/tokenSearch/helpers/graphqlClients.js
+++ src/tokenSearch/helpers/graphqlClients.js
@@ -1,6 +1,5 @@
 import { GraphQLClient } from 'graphql-request';
 
-import { romeTokenSyncUri, tokenSyncUri } from './config';
+import { romeTokenSyncUri, } from './config';
 
-export const veloxPairsClient = new GraphQLClient(tokenSyncUri);
 export const romePairsClient = new GraphQLClient(romeTokenSyncUri);
