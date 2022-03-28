e0e48ee0cc3305415063ae7e42a00ea6df2c0c73
Mark Camball <mark.camball@gmail.com>
2022-03-28 14:15:52 -0400

    dynamic filter networks and exchanges

--- src/tokenSearch/SearchFiltering.jsx
+++ src/tokenSearch/SearchFiltering.jsx
@@ -1,51 +1,42 @@
 import React, { useState } from "react"
+import {uniq, uniqBy} from "lodash"
+import {networkExchangePairs} from "./helpers/config";
 
-const NetworkSelection = ({networkName, setNetwork, network}) =>{
+const NetworkSelection = ({networkName, setNetworkMap, networkMap}) =>{
 
   return <>
-    <input checked={network==networkName} type="radio" onClick={()=>setNetwork(networkName)}/>
+    <input checked={networkMap[networkName] || false} type="checkbox" onChange={(e)=>setNetworkMap(networkMap=>({
+      ...networkMap,
+      [networkName]:e.target.checked
+    }))}/>
+
     {networkName}
   </>
 }
 
 export const SearchFiltering = () => {
-  const [network, setNetwork ] = useState()//todo link into redux state
+  const [networkMap, setNetworkMap] = useState({})//todo link into redux state
+  const [exchangeMap, setExchangeMap] = useState({})//todo link into redux state
 
   return (
     <div style={{border: "solid"}}>
       <div style={{width: "100%", height: "40px"}}>Select blockchain</div>
-      <NetworkSelection setNetwork={setNetwork} network={network} networkName={"All"}/>
-      <NetworkSelection setNetwork={setNetwork} network={network} networkName={"Ethereum"}/>
-      <NetworkSelection setNetwork={setNetwork} network={network} networkName={"Avalanche"}/>
-      <NetworkSelection setNetwork={setNetwork} network={network} networkName={"BSC"}/>
+      {
+        uniq(networkExchangePairs.map(pair=>pair[0])).map(networkName => {
+          return <NetworkSelection key={networkName} setNetworkMap={setNetworkMap} networkMap={networkMap} networkName={networkName}/>
+        })
+      }
       <hr/>
-
-      {network==="Ethereum" && <>
-        <input type="checkbox"/>
-        All exchanges
-        <input type="checkbox"/>
-        UniswapV3
-        <input type="checkbox"/>
-        SushiSwap
-      </>}
-
-      {network==="Avalanche" && <>
-        <input type="checkbox"/>
-        All exchanges
-        <input type="checkbox"/>
-        Pangolin
-        <input type="checkbox"/>
-        TraderJoe
-      </>}
-
-      {network==="BSC" && <>
-        <input type="checkbox"/>
-        All exchanges
-        <input type="checkbox"/>
-        PancakeSwap
-        <input type="checkbox"/>
-        MDex
-      </>}
+      {uniqBy(networkExchangePairs.filter(pair=>networkMap[pair[0]]),1).map(pair=>{
+        const exchangeName = pair[1]
+        return <div key={exchangeName}>
+          <input  checked={exchangeMap[exchangeName] || false} type="checkbox" onChange={(e)=>setExchangeMap(exchangeMap=>({
+            ...exchangeMap,
+            [exchangeName]:e.target.checked
+          }))}/>
+          {pair[1]}
+        </div>
+      })}
     </div>
   )
 }
--- src/tokenSearch/helpers/config.js
+++ src/tokenSearch/helpers/config.js
@@ -3,3 +3,42 @@ export const romeTokenSyncUri = String(
   process.env.REACT_APP_HASURA_API_ENDPOINT_WS ||//todo should be a prop or something
     'https://romenet.prod.velox.global/v1/graphql'
 ).replace('ws', 'http');
+
+
+
+const AvalanchePairs = [
+  ["avalanche","baguette"],
+  ["avalanche","canary"],
+  ["avalanche","complusnetwork"],
+  ["avalanche","elkfinance"],
+  ["avalanche","kyberdmm"],
+  ["avalanche","lydiafinance"],
+  ["avalanche","oliveswap"],
+  ["avalanche","pandaswap"],
+  ["avalanche","pangolin"],
+  ["avalanche","sushiswap"],
+  ["avalanche","traderjoe"],
+  ["avalanche","yetiswap"],
+  ["avalanche","zeroexchange"],
+]
+const BSCPairs = [
+  ["bsc","apeswap"],
+  ["bsc","babyswap"],
+  ["bsc","biswap"],
+  ["bsc","ellipsis.finance"],
+  ["bsc","mdex"],
+  ["bsc","pancakeswap"],
+  ["bsc","safeswap"],
+  ["bsc","sushiswap"],
+]
+const moonbeamPairs = [
+  ["moonbeam","beamswap"],
+  ["moonbeam","solarflare"],
+  ["moonbeam","stellaswap"],
+  ["moonbeam","sushiswap"],
+]
+const moonriverPairs = [
+  ["moonriver","solarbeam"],
+  ["moonriver","sushiswap"],
+]
+export const networkExchangePairs = [...BSCPairs,...AvalanchePairs, ...moonbeamPairs, ...moonriverPairs]
