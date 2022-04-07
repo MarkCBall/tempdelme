12fa698d397fea5c7113bab08c436335990cec64
oneway <trust.biz888@gmail.com>
2022-04-07 09:31:06 -0500

    styling

--- src/tokenSearch/SearchResult.jsx
+++ src/tokenSearch/SearchResult.jsx
@@ -3,26 +3,7 @@ import 'twin.macro';
 import 'styled-components/macro'
 
 import { useSelector } from 'react-redux';
-import styled from 'styled-components';
 import TokenPairDetail from './TokenPairDetail';
-
-const NilFoundContainer = styled.div`
-  width: 50%;
-  margin-left: 25%;
-  margin-right: 25%;
-  margin-top: -5px;
-  position: relative;
-  background-color: #1c646c;
-  z-index: 100;
-  color: rgba(0, 0, 0, 0.87);
-  height: 60px;
-  text-align: center;
-  color: white;
-  font-weight: bolder;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
 
 const SearchResult = (props) => {
   const {suggestions, searchText} = useSelector(
@@ -33,14 +14,14 @@ const SearchResult = (props) => {
     .sort((pair1, pair2) => pair2.volumeUSD - pair1.volumeUSD);
 
   if (props.loading) {
-    return <NilFoundContainer>Loading...</NilFoundContainer>;
+    return <div tw="relative flex bg-white justify-center items-center">Loading...</div>;
   }
 
   if (!!searchText && !filteredSuggestions.length) {
-    return <NilFoundContainer>No pairs found...</NilFoundContainer>;
+    return <div tw="relative flex bg-white justify-center items-center">No pairs found...</div>;
   }   
 
-  return (
+  return (    
     <div tw="h-60 overflow-y-auto pl-4 border-solid">    
       {
         filteredSuggestions.map((suggestions, index) => 
@@ -50,7 +31,7 @@ const SearchResult = (props) => {
         />
         )
       }  
-    </div>    
+    </div>        
   );
 };
 export default SearchResult;
