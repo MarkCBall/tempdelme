6ad59b7c30751e315dc68fc43b814cf99b6d6707
CP2053 <guillaume@romeblockchain.com>
2022-04-14 16:24:09 -0400

    Replacing previous branch to remove npm link uncollected garbage

--- src/tokenSearch/SearchInput.jsx
+++ src/tokenSearch/SearchInput.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
-import { searchTokenPairs, startSelecting, toggleSelecting, setSearchText, setSearchToken, setDebounce } from '../redux/tokenSearchSlice';
+import { searchTokenPairs, startSelecting, toggleSelecting, setSearchText, setDebounce } from '../redux/tokenSearchSlice';
 import magnifyingGlass from './icon-search.svg';
+import { mutatorTextToken } from "./helpers/mutatorTextToken";
 
 
 const PairField = styled.div`
@@ -55,67 +56,6 @@ const SearchInput = () => {
   const { searchText, networkMap, exchangeMap, searchToken, searchDebounceDelay } = useSelector((state) => state);
 
 
-  // Function that validates if the user is searching for a token and changes the search string accordingly.
-  // We are also adding a value to say if the token search was enabled or not, that way if the user removes characters from the string outside of the threshold,
-  // we will revert to a non-token search.
-  //
-  // IMPORTANT: If the user is searching for a string, that is NOT a token but validates as one, the search WILL only be looking for a token.
-  //
-  const searchTokenValidation = input => {
-    // We want to tell the user whats going on by updating the value on the UI.
-    // The user has to know that we will be looking only for tokens that start with the said string now.
-    let value = input.value;
-    let leadIsTokenLike = value.substr(0, 2).toLowerCase() === '0x';
-    let valueIsTokenLike = new RegExp(/^[0-9a-f]+$/i).test(value.substr(leadIsTokenLike ? 2 : 0));
-    let valueLength = value.length;
-
-
-    // Checks that 'leadIsTokenLike' is false and that the value is token like.
-    if (!leadIsTokenLike && valueIsTokenLike && valueLength >= 5) {
-      // Add the leading '0x' to the value.
-      value = '0x' + value;
-
-      // User is looking for a token.
-      dispatch(setSearchToken(true));
-    }
-    else
-      // Checks if the search is looking for a token.
-      if (searchToken)
-        // Checks that the value lead is token like.
-        if (leadIsTokenLike) {
-          // Checks if the user has deleted characters from the search threshold for token detection.
-          // We are looking for a lenght smaller than 7, since we have to take into account the leading '0x'.
-          if (valueLength < 7) {
-            // Removes the leading '0x' from the value.
-            value = value.substr(2);
-
-            // User is NOT looking for a token.
-            dispatch(setSearchToken(false));
-          }
-          else
-            // We are checking if the value is token like; since the user may have added non-hex characters to the search string.
-            if (!valueIsTokenLike) {
-              // Removes the leading '0x' from the value.
-              value = value.substr(2);
-
-              // User is NOT looking for a token.
-              dispatch(setSearchToken(false));
-            }
-        }
-        // This should not happen.
-        // Something went wrong, not sure how this case can happen, but we are turning off the token search.
-        else
-          // User is NOT looking for a token.
-          dispatch(setSearchToken(false));
-
-    // We update the input value if the string is not empty.
-    if (!value) input.value = value;
-
-    // Returning.
-    return value;
-  };
-
-
   // Updates the datasets of the results.
   useEffect(
     () => dispatch(setDebounce(
@@ -132,7 +72,7 @@ const SearchInput = () => {
         placeholder={'Select a token pair'}
         autocomplete={'off'}
         maxLength="48"
-        onChange={e => dispatch(setSearchText({ text: searchTokenValidation(e.target), debounceDelay: 450 }))}
+        onChange={e => dispatch(setSearchText({ text: mutatorTextToken(e.target, searchToken, dispatch), debounceDelay: 450 }))}
       />
       <HideOnSmallScreen
         alt={''}
